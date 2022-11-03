20fd377e6b28f7e25543c765b9a7724b5b8ba319
Davi Chagas <davi.almchagas@gmail.com>
2022-11-03 09:30:55 -0300

    Default from nex.js

--- src/Assets/icons/index.js
+++
@@ -1,6 +0,0 @@
-const icons = {
-    whatsapp: require('./whatsapp.svg'),
-    terrazoDiDante: require('./terrazo_di_dante.svg')
-}
-
-export default icons
--- src/components/Carousel/index.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Carousel = styled.div `
-
-`
-
-export default Carousel
