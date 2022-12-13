8af96450d0ebc83ee59215eda8e420a16b8e3e34
Davi Chagas <davi.almchagas@gmail.com>
2022-12-13 17:54:51 -0300

    New alt for the carousel images

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -20,7 +20,7 @@ function Base({className, path, numberOfImages}) {
 return (
     <div className={className}>
 
-        <Image src={`${path}(${index}).jpeg`} Alt="s"  width="1600" height="1200" />
+        <Image src={`${path}(${index}).jpeg`} alt="Carousel image"  width="1600" height="1200" />
 
         <button id="Prev" onClick={subtractIndex}>
             <Image src={icons.arrow}
