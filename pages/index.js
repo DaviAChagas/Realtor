16d7e024b5ed2ec0606c2be3df10ea7ab30cde45
Davi Chagas <davi.almchagas@gmail.com>
2022-12-13 22:48:59 -0300

    Instaled vercel analytics

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,30 @@ import Wrapper from '../components/Wrapper'
 import Header from '../components/Header'
 import Card from '../components/Card'
 import db from '../db.json'
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
  return ( 
+
 <Wrapper rowGap="2" bgColor="#f7ffd7">
-<Header />
 
-<section className="Cards">
-{db.map((local) => {
-return (
-  <Card 
-  key={local.name}
-  name={local.name}
-  background={local.background}
-  city={local.localization.city}
-  state={local.localization.state}/>
-  )
+  <Header />
+
+  <section className="Cards">
+    {db.map((local) => {
+      return (
+        <Card 
+        key={local.name}
+        name={local.name}
+        background={local.background}
+        city={local.localization.city}
+        state={local.localization.state}/>
+              )
 })}
-</section>
+   </section>
 
+        <Analytics />
      </Wrapper>
  )
 }
